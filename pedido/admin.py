84a0872184f18189e82bf77283f55dd554f88559
staviasz <erickesc21@hotmail.com>
2023-06-28 12:02:32 -0300

    desenvolvimento dos models pedido e item do pedido

--- pedido/admin.py
+++ pedido/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from pedido.models import ItemPedido, Pedido
 
-# Register your models here.
+
+class ItemPedidoInline(admin.TabularInline):
+  model = ItemPedido
+  extra = 1
+
+
+class PedidoAdmin(admin.ModelAdmin):
+  inlines = ItemPedidoInline,
+
+admin.site.register(Pedido, PedidoAdmin)
